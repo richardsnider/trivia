849617ca934802d4ae87736c2deef82f5fcf09bc
Richard Snider <richardnsnider@gmail.com>
2018-02-08 16:26:04 -0800

    Add webpack compiler

---
+++ script/resource/webpack-compiler.js
@@ -0,0 +1,30 @@
+const path = require(`path`);
+const dotenvWebpack = require(`dotenv-webpack`);
+const webpack = require(`webpack`);
+
+module.exports = webpack({
+    entry: `./src/app.js`,
+    output: {
+        path: path.join(__dirname, `../../dist`),
+        filename: `app.bundle.js`,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: `babel-loader`,
+                    options: {
+                        presets: [`env`]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new dotenvWebpack({
+            path: `./.env`
+        })
+    ]
+});
