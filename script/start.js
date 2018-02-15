e80aa033b560801dbed5b6e7fc21de4be337a569
Richard Snider <richardnsnider@gmail.com>
2018-02-15 10:28:25 -0800

    Stop running watcher in deployments

--- script/start.js
+++ script/start.js
@@ -9,30 +9,39 @@ const webpackCompiler = require(`./resource/webpack-compiler.js`);
 
 builder.build();
 
-const watcher = webpackCompiler.watch({
-    ignored: [/node_modules/, /dist/],
-    poll: 1000
-}, (err, stats) => {
-    console.log(moment().format(`M/D/YY HH:mm:ss`) + ` Changes detected!`);
-    if (server.instance) {
-        console.log(`Closing server.`);
-        server.close();
-    }
-    console.log(`Starting server.`);
-    server.start();
-});
+if (process.env.NODE_ENV !== `local`) {
+    webpackCompiler.run(function (err, stats) {
+        if (err) throw err;
+        server.start()
+    });
+}
+else {
+    const watcher = webpackCompiler.watch({
+        ignored: [/node_modules/, /dist/],
+        poll: 1000
+    }, (err, stats) => {
+        console.log(moment().format(`M/D/YY HH:mm:ss`) + ` Changes detected!`);
+        if (server.instance) {
+            console.log(`Closing server.`);
+            server.close();
+        }
+        console.log(`Starting server.`);
+        server.start();
+    });
+    console.log(`Watcher started.`);
 
-process.on(`SIGINT`, () => {
-    console.log(`Closing server...`)
-    watcher.close(() => { console.log(`Watching Ended.`); });
-  
-    server.close(() => {
-      console.log(`Server closed.`)
-      process.exit()
+    process.on(`SIGINT`, () => {
+        console.log(`Closing server...`)
+        watcher.close(() => { console.log(`Watching Ended.`); });
+
+        server.close(() => {
+            console.log(`Server closed.`)
+            process.exit()
+        })
+
+        setTimeout((e) => {
+            console.log(`Forcing server close !!!`, e)
+            process.exit(1)
+        }, 5000)
     })
-  
-    setTimeout((e) => {
-      console.log(`Forcing server close !!!`, e)
-      process.exit(1)
-    }, 5000)
-  })
+}
