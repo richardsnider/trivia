629de179ad334099d95786c498e40ecfe2fa20ba
Richard Snider <richardnsnider@gmail.com>
2018-02-15 14:16:05 -0800

    Add a stylesheet

--- script/resource/builder.js
+++ script/resource/builder.js
@@ -10,6 +10,8 @@ module.exports = {
             .pipe(fs.createWriteStream(`./dist/index.html`));
         fs.createReadStream(`./src/media/favicon.ico`)
             .pipe(fs.createWriteStream(`./dist/favicon.ico`));
+        fs.createReadStream(`./src/stylesheet.css`)
+            .pipe(fs.createWriteStream(`./dist/stylesheet.css`));
 
         console.log(`Building completed.`)
     }
