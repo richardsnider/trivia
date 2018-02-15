dbfb8da511f4e54d628a2d3826ae50bf2c93a7c2
Richard Snider <richardnsnider@gmail.com>
2018-02-15 13:12:26 -0800

    Fix websocket client

--- src/websocket.js
+++ src/websocket.js
@@ -4,34 +4,37 @@ import moment from 'moment';
 import io from 'socket.io-client';
 import { setInterval, setTimeout } from 'timers';
 
-const websocket = {
+const client = {
     init: function () {
-        this.socket = io(process.env.HOST + `:` + process.env.HTTP_PORT);
+        client.socket = io(process.env.HOST + `:` + process.env.HTTP_PORT);
 
-        this.socket.emit(`question`);
+        client.socket.emit(`question`);
 
-        this.socket.on(`question`, function (data) {
+        client.socket.on(`question`, function (data) {
             let i = data[Symbol.iterator]();
             let currentTimer;
             (function changeTextContent() {
                 let next = i.next();
-                home.category.textContent = next.value.category;
-                home.question.textContent = next.value.question;
-                home.answer.textContent = next.value.answer;
-                home.answer.style.opacity = 0;
-                let answerTimer = setTimeout(function() {
-                    home.answer.style.opacity = 1.0;
-                }, 10000)
-                let timeout = next.done === false ? changeTextContent : function() { socket.emit(`question`); };
+                if (!next.done) {
+                    home.category.textContent = next.value.category;
+                    home.question.textContent = next.value.question;
+                    home.answer.textContent = next.value.answer;
+                    home.answer.style.opacity = 0;
+                    let answerTimer = setTimeout(function () {
+                        home.answer.style.opacity = 1.0;
+                    }, 10000)
+                }
+
+                let timeout = next.done === false ? changeTextContent : function () { client.socket.emit(`question`); };
                 currentTimer = setTimeout(timeout, 15000)
             })();
         });
 
-        this.socket.on(`disconnect`, function(data) {
+        client.socket.on(`disconnect`, function (data) {
             console.log(`Host websocket has disconnected...`)
             socket.disconnect();
         });
     }
 };
 
-export default websocket;
+export default client;
