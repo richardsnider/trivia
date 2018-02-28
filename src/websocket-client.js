8063427cb9997bf1d25abb7f11fc91f84d6bc557
Richard Snider <richardnsnider@gmail.com>
2018-02-28 09:31:36 -0800

    Improve websocket client logging and handling

--- src/websocket-client.js
+++ src/websocket-client.js
@@ -1,18 +1,22 @@
 import home from './home.js';
+import io from 'socket.io-client';
+import log from './utility/log.js';
 import map from 'lodash.map';
 import moment from 'moment';
-import io from 'socket.io-client';
-import { setInterval, setTimeout } from 'timers';
+import timers from 'timers';
 
 const client = {
     init: function () {
         client.socket = io(process.env.HOST + `:` + process.env.HTTP_PORT);
 
+        client.socket.on(`connect`, function (data) {
+            log(`Websocket ` + client.socket.id + ` connected to namespace: ` + client.socket.nsp);
+        });
+
         client.socket.emit(`question`);
 
         client.socket.on(`question`, function (data) {
             let i = 0;
-            let questionTimer, answerTimer, opacityTimer;
 
             function changeTextContent() {
                 if (i >= data.length) {
@@ -24,9 +28,8 @@ const client = {
                 home.question.textContent = data[i].question;
                 home.answer.style.opacity = 0;
                 home.answer.textContent = data[i].answer;
-
-                answerTimer = setTimeout(showAnswer, 30000);
-                questionTimer = setTimeout(changeTextContent, 40000);
+                client.answerTimer = timers.setTimeout(showAnswer, 30000);
+                client.questionTimer = timers.setTimeout(changeTextContent, 40000);
                 i++;
             }
 
@@ -38,7 +41,9 @@ const client = {
         });
 
         client.socket.on(`disconnect`, function (data) {
-            console.log(`Host websocket has disconnected...`)
+            log(`Host websocket has disconnected.`);
+            timers.clearTimeout(client.answerTimer);
+            timers.clearTimeout(client.answerTimer);
             socket.disconnect();
         });
     }
